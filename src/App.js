7c04ed77fa830b81a455c69c5b6997f72477e1f9
kkrasnoff <kkrasnoff@TOPSPINSEC.COM>
2019-03-06 10:13:47 +0200

    initial form buid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      txtUserA: "",
+      txtUserB: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleKeyPress = (event) => {
+    if(event.key === 'Enter'){
+      console.log('enter press here! ')
+
+    }
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container chatWindow">
+        <div className="row">
+          <div className="col-sm">
+            messages
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm">
+            <input type="text" id="txtUserA" onChange={this.handleChange} onKeyPress={this.handleKeyPress} value={this.state.txtUserA} />
+          </div>
+          <div className="col-sm">
+            <input type="text" id="txtUserB" onChange={this.handleChange} onKeyPress={this.handleKeyPress} value={this.state.txtUserB} />
+          </div>
+        </div>
       </div>
     );
   }
